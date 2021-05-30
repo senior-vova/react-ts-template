2b77f9d059f33a475bec949d40d701d4207427b9
senior-vova <vladimir.reg.acc@gmail.com>
2021-05-30 14:41:33 +0400

    create routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from "react";
+import { Route, Switch } from "react-router";
 import { MainAppHOC } from "./hoc/MainAppHOC";
+import HomePage from "./pages/home";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/" render={() => <HomePage />} />
+    </Switch>
   );
 };
 
